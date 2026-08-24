4fc72165747a3232a1cb1cdb2ac068b032ace132
Sanjay-Edoble <sanjay.edoble@gmail.com>
2026-08-25 01:41:02 +0530

    Fix Share view path causing 500 error

--- app/Controllers/ShareController.php
+++ app/Controllers/ShareController.php
@@ -16,7 +16,7 @@ class ShareController
         $formUrl = url("/f/{$params['id']}");
         $embedCode = '<iframe src="' . url("/embed/{$params['id']}") . '" width="100%" height="600" frameborder="0" style="border:0;border-radius:8px;" allowfullscreen></iframe>';
 
-        echo view('share.index', [
+        echo view('forms.share', [
             'pageTitle'  => 'Share: ' . ($form['title'] ?? 'Untitled'),
             'form'       => $form,
             'formUrl'    => $formUrl,
